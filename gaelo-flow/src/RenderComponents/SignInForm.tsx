e2d0251beb05e06ef859ad7a4bd3246045ceb101
remixxx31 <remiperray@gmail.com>
2023-12-03 21:11:17 +0100

    feat: component SignInForm and bg-gradient

---
+++ gaelo-flow/src/RenderComponents/SignInForm.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+export const SignInForm = () => {
+    const [, setUsername] = useState('');
+    const [, setMobileNumber] = useState('');
+
+    const handleConnect = (event: React.FormEvent) => {
+        event.preventDefault();
+        // Logique de connexion ici
+    };
+
+    return (
+        <form onSubmit={handleConnect} className="bg-white p-8 rounded-lg shadow-md">
+            <div className="mb-4">
+                <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">Username</label>
+                <input type="text" id="username" onChange={(e) => setUsername(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+            </div>
+            <div className="mb-4">
+                <label htmlFor="mobileNumber" className="block text-gray-700 text-sm font-bold mb-2">Mobile Number</label>
+                <input type="tel" id="mobileNumber" onChange={(e) => setMobileNumber(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+            </div>
+            <div className="flex items-center justify-between">
+                <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                    Connect
+                </button>
+            </div>
+        </form>
+    );
+};
+
--- gaelo-flow/tailwind.config.js
+++ gaelo-flow/tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "custom-gradient":
+          "linear-gradient(90deg, #343375 6.36%, rgba(98, 71, 101, 0.96) 33.54%, rgba(202, 135, 77, 0.99) 66.93%, #FFA840 73.7%, rgba(251, 165, 65, 1.00) 75.92%)",
+      }),
+    },
   },
-  plugins: [],
-}
+  plugins: ["prettier-plugin-tailwindcss"],
+};
 
+//TODO : adapt gradient to looks like the figma
