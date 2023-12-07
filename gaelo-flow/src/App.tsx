806f5c33825be8c7af53b4eb271986a69d0dc146
Sophie Labyt <labytsophie@gmail.com>
2023-12-07 10:38:52 +0100

    add images

--- gaelo-flow/src/App.tsx
+++ gaelo-flow/src/App.tsx
@@ -4,9 +4,12 @@ import './index.css'
 function App() {
 
   return (
-<div className='h-screen w-screen columns-2 gap-0 bg-gradient-to-r from-indigo-700 to-amber-500'>
-      <div className="h-full w-full " >
-
+    <div className='h-screen w-screen columns-2 gap-0 bg-gradient-to-r from-indigo-700 to-amber-500'>
+      <div className="h-full w-full" >
+        <img src="/gaelo-flow-white.png" className='absolute top-0 left-0 w-1/6'></img>
+        <div className="flex h-screen w-full justify-center items-center">
+          <img src="/gaelo-flow.png" className='absolute w-1/4'></img>
+        </div>
       </div>
       <div className="h-full w-full flex justify-center items-center bg-white rounded-tl-3xl" style={{ filter: 'drop-shadow(-20px 0 20px rgba(50, 50, 50, 0.5))' }}>
         <SignInForm />
@@ -16,5 +19,3 @@ function App() {
 }
 
 export default App
-
-// TODO : Fix top cotnrt left of right section muste be rounded (70-75%)
