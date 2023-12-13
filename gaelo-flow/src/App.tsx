59c6d3597b2ed4a940f3ee79a0860d392871c8bc
Salim Kanoun <salim.kanoun@gmail.com>
2023-12-13 14:04:48 +0000

    fiuxes

--- gaelo-flow/src/App.tsx
+++ gaelo-flow/src/App.tsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ErrorBoundary } from "react-error-boundary";
 import Welcome from "./Welcome";
 import "./index.css";
 import Button from "./RenderComponents/Button";
@@ -8,63 +7,51 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { store } from "./store";
+import ErrorBoundary from "./ErrorBoundary";
 
 
 const queryClient = new QueryClient();
 
 function App() {
-  // Fonction à appeler en cas d'erreur
-  const handleError = (error, errorInfo) => {
-    // Logique de gestion d'erreurs (par exemple, envoi à un service de suivi des erreurs)
-    console.error("Erreur interceptée par ErrorBoundary", error, errorInfo);
-  };
-
-  // Composant à afficher en cas d'erreur
-  const ErrorFallback = ({ error, resetErrorBoundary }) => (
-    <div role="alert">
-      <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Réessayer</button>
-    </div>
-  );
 
   const [displayLegalMention, setDisplayLegalMention] =
     useState<boolean>(false);
 
   return (
     <Provider store={store}>
-      <QueryClientProvider 
-      client={queryClient}
+      <QueryClientProvider
+        client={queryClient}
       >
-      <ErrorBoundary FallbackComponent={ErrorFallback} onError={handleError}>
-        <BrowserRouter>
-          <Routes>
-            <Route
+        <ErrorBoundary FallbackComponent={<>Error</>}>
+          <BrowserRouter>
+            <Routes>
+              <Route
 
-              path="/legal-mention"
-              element={
-                <>
-                  <Button
-                    onClick={() => setDisplayLegalMention(false)}
-                    color="purple"
-                  >
-                    Return to Main
-                  </Button>{" "}
-                  "Legal Mention"
-                </>
-              }
-            />
-            <Route
-              path="/"
-              element={
-                <Welcome onLegalMention={() => setDisplayLegalMention(true)} />
-              }
-            />
-          </Routes>
-          {displayLegalMention && <Button onClick={() => setDisplayLegalMention(false)} color="purple">Return to Main</Button>}
-        </BrowserRouter>
-      </ErrorBoundary>
-      <ReactQueryDevtools initialIsOpen={true} />
-    </QueryClientProvider>
+                path="/legal-mention"
+                element={
+                  <>
+                    <Button
+                      onClick={() => setDisplayLegalMention(false)}
+                      color="purple"
+                    >
+                      Return to Main
+                    </Button>{" "}
+                    "Legal Mention"
+                  </>
+                }
+              />
+              <Route
+                path="/"
+                element={
+                  <Welcome onLegalMention={() => setDisplayLegalMention(true)} />
+                }
+              />
+            </Routes>
+            {displayLegalMention && <Button onClick={() => setDisplayLegalMention(false)} color="purple">Return to Main</Button>}
+          </BrowserRouter>
+        </ErrorBoundary>
+        <ReactQueryDevtools initialIsOpen={true} />
+      </QueryClientProvider>
     </Provider>
   );
 }
--- gaelo-flow/src/auth/SignInForm.tsx
+++ gaelo-flow/src/auth/SignInForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import Button from '../RenderComponents/Button';
 import Input from '../RenderComponents/Input';
 import { useCustomMutation } from '../utils/reactQuery';
@@ -21,8 +21,7 @@ export const SignInForm = () => {
         [],
         {
             onSuccess: (data: Record<string, any>) => {
-                const decodedToken = jwtDecode(data.token);
-                console.log(decodedToken)
+                const decodedToken  : Record<string, any>= jwtDecode(data.token);
                 dispatch(login({ token: data.token, userId: decodedToken.userId }));
             },
             onError: () => {
@@ -40,12 +39,12 @@ export const SignInForm = () => {
             <p className="text-lg text-gray-700 text-center mb-8">Please Log in to your Account.</p>
             <div className="mb-4 w-full">
                 <label className="block text-gray-00 font-bold mb-3" htmlFor="username">Username:</label>
-                <Input bordered placeholder="Enter your username" value={username} onChange={(event) => setUsername(event.target.value)} />
+                <Input bordered placeholder="Enter your username" value={username} onChange={(event :ChangeEvent<HTMLInputElement>) => setUsername(event.target.value)} />
             </div>
 
             <div className="mb-4">
                 <label className="block text-gray-900 text-sm font-bold mb-3" htmlFor="password">Password:</label>
-                <Input bordered placeholder="Enter your password" value={password} onChange={(event) => setPassword(event.target.value)} type="password" />
+                <Input bordered placeholder="Enter your password" value={password} onChange={(event :ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)} type="password" />
                 <div className="w-full flex justify-center">
                     <Button color="purple" onClick={onLogin} bordered disabled={username.length === 0 || password.length === 0}>Connect</Button>
                 </div>
