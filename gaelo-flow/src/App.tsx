02ed4575b7b9410198ae95e1a94c5a2507a3a054
Salim Kanoun <salim.kanoun@gmail.com>
2023-12-13 14:07:24 +0000

    update

--- gaelo-flow/src/App.tsx
+++ gaelo-flow/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Welcome from "./Welcome";
 import "./index.css";
