a7a6e3e38389ec7c4bb045d14bbc4f408e29c613
remixxx31 <remi.perray@epitech.eu>
2023-12-14 16:28:18 +0100

    feat:NavBar feat in progress

--- gaelo-flow/src/Home.tsx
+++ gaelo-flow/src/Home.tsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { logout } from "./reducers/UserSlice";
 import { useNavigate } from "react-router-dom";
 import Button from "./RenderComponents/Button";
+import SideBar from "./RenderComponents/NavBar/SideBar";
 
 
 
@@ -17,6 +18,7 @@ const Home = () => {
     }
     return (
         <div>
+            <SideBar/>
             <h1 className="text-4xl font-bold text-center mb-8">Welcome !</h1>
             <p className="text-lg text-gray-700 text-center mb-8">You are connected.</p>
             //TODO : redirect route for logout aftre clicked on button logout
