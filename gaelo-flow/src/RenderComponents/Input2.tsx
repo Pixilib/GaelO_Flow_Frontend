32e65e295d93ace4a595fe7602cc45f1b5735c45
remixxx31 <remi.perray@epitech.eu>
2024-01-03 16:13:32 +0100

    Refactor Input2 component and update tailwind.config.js

--- gaelo-flow/src/RenderComponents/Input2.tsx
+++ gaelo-flow/src/RenderComponents/Input2.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 type InputProps = {
   className?: string;
@@ -9,7 +9,14 @@ type InputProps = {
   [key: string]: any;
 };
 
-const Input2 = ({ className = "", placeholder = "", label = "", svgLeft = null, svgRight = null, ...props }: InputProps) => {
+const Input2 = ({
+  className = "",
+  placeholder = "",
+  label = "",
+  svgLeft = null,
+  svgRight = null,
+  ...props
+}: InputProps) => {
   const [isFocused, setIsFocused] = useState(false);
 
   const handleFocus = () => setIsFocused(true);
@@ -18,20 +25,25 @@ const Input2 = ({ className = "", placeholder = "", label = "", svgLeft = null, 
   return (
     <div className={`relative flex flex-col ${className}`}>
       {label && (
-        <label 
+        <legend
           className={`transition-all duration-100 ease-in-out 
-                      ${isFocused 
-                        ? 'text-xs text-gray-800 absolute -top-2 left-2 bg-white px-1' 
-                        : 'text-base text-gray-500 -top-1 left-0'}`
-          }
+                      ${
+                        isFocused
+                          ? "text-xs text-gray-800 absolute -top-2 left-5 bg-white px-1"
+                          : "font-bold mt-3 -top-4 left-0"
+                      }`}
         >
           {label}
-        </label>
+        </legend>
       )}
       <input
         type="text"
-        placeholder={isFocused ? '' : placeholder}
-        className={"pl-4 pr-12 py-3.5 bg-white text-gray-800 w-full text-sm border-2 border-gray-300 rounded outline-none focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-700 dark:border-transparent dark:text-gray-400 dark:focus:ring-gray-600" + " " + className}
+        placeholder={isFocused ? "" : placeholder}
+        className={
+          "pl-4 pr-12 py-3.5 bg-gray-100  focus:bg-white focus:back text-gray-800 w-full text-sm border-2 border-gray-300 rounded-lg outline-none focus:border-purpleCustom focus:ring-purpleCustom disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-700 dark:border-transparent dark:text-gray-400 dark:focus:ring-gray-600" +
+          " " +
+          className
+        }
         onFocus={handleFocus}
         onBlur={handleBlur}
         {...props}
--- gaelo-flow/tailwind.config.js
+++ gaelo-flow/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        'purpleCustom':'#4746B8'
+      },
     },
   },
   plugins: ["prettier-plugin-tailwindcss"],
