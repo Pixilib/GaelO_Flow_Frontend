338291274cf62163f980a95bd0a0574cdcdfd5f2
Sophie Labyt <labytsophie@gmail.com>
2024-01-10 16:57:46 +0100

    style storiescards

--- gaelo-flow/src/stories/Exemples/Card.stories.tsx
+++ gaelo-flow/src/stories/Exemples/Card.stories.tsx
@@ -1,29 +1,38 @@
-import { Meta, StoryFn } from "@storybook/react";
 import React from "react";
-import { Card, Header } from "../../RenderComponents/Card";
+import { Meta, Story } from "@storybook/react";
+import Card from "../../RenderComponents/Card";
+import ChevronRight from "../../assets/chevron-right.svg";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-    title: "Gaelo FLow UI/Card",
-    component: Card,
-    argTypes: {
-        disabled: {
-            options: [true, false],
-            control: { type: "radio" },
-            description: "Choose disabled state",
-        },
-        
+  title: "Gaelo FLow UI/Card",
+  component: Card,
+  args: {
+    color: "primary",
+    bordered: false,
+  },
+  argTypes: {
+    children: {
+      options: ["Text", <ChevronRight />],
+      control: { type: "radio" },
     },
-} as Meta<typeof Card>;
+    disabled: {
+      options: [true, false],
+      control: { type: "radio" },
+    },
+  },
+} as Meta;
 
-const template_button: StoryFn<typeof Card> = (args) => {
-    console.debug(args);
-    return (
-      <Card color={args.color}>
-        <Header />
-      </Card>
-    );
-  };
+const Template: Story = (args) => (
+  <Card color={args.color}>
+    <Card.BasicHeader title="Card Title" />
+    <Card.Body>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, facilis non a dolore soluta consectetur modi minima corrupti id ab at ipsa, vel fugit magnam, numquam aperiam dicta doloribus! Harum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, facilis non a dolore soluta consectetur modi minima corrupti id ab at ipsa, vel fugit magnam, numquam aperiam dicta doloribus! Harum
+    </Card.Body>
+    <Card.Footer>
+      {/* Your Footer content goes here */}
+      Footer content
+    </Card.Footer>
+  </Card>
+);
 
-      
-export const template_buttons = template_button.bind({});
+export const template_buttons = Template.bind({});
