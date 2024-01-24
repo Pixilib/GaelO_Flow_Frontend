c85639198f7fec418d1db9cf09630584566bfbb9
remixxx31 <remi.perray@epitech.eu>
2024-01-24 11:52:17 +0100

    style: :lipstick: Update NavBar styling

--- src/RenderComponents/NavBar/SideBar.tsx
+++ src/RenderComponents/NavBar/SideBar.tsx
@@ -40,7 +40,7 @@ export const SideBar = () => {
 
   return (
     <nav data-gaelo-flow="sidebar" className="w-full h-full bg-background">
-      <div className="h-full rounded-tr-3xl top-0 start-0 bottom-0 z-[60] w-64 bg-primary border-e  pt-7 pb-10 overflow-y-hidden lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500 dark:bg-gray-800 px-6">
+      <div className="h-full rounded-tr-3xl top-0 start-0 bottom-0 z-[60] w-64 bg-primary pt-7 pb-10 overflow-y-hidden">
         <LogoSideBar className="mx-auto mb-10" />
         <MenuItem
           title="Administration"
@@ -51,7 +51,6 @@ export const SideBar = () => {
         <MenuItemsCollapse
           icon={<Administrator />}
           title="Authentification"
-          className="bg-secondary"
           items={authItems}
           isOpen={openItem === "Authentification"}
           toggleOpen={() => toggleOpen("Authentification")}
