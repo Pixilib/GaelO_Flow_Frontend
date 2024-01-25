c97a573914fec7d278ef88706986571d31fcec4a
Sophie Labyt <labytsophie@gmail.com>
2024-01-25 15:39:20 +0100

    update App.tsx, Card.tsx and General.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Welcome from "./Welcome";
 import "./index.css";
 import { RootState } from "./store";
 import RootApp from "./root/RootApp";
+import General from "./admin/General";
 
 
 
@@ -10,7 +11,10 @@ import RootApp from "./root/RootApp";
 function App() {
   const isLogged = useSelector((state: RootState) => state.user.isLogged);
   
+return (
 
+  <General/>
+)
   if (isLogged) return <RootApp />;
   else return <Welcome />;
 
--- src/admin/General.tsx
+++ src/admin/General.tsx
@@ -9,51 +9,56 @@ const General = () => {
 
   return (
     <div className="flex-col h-screen p-8 bg-background">
-      {/* Card Redis Setting */}
-      <Card className="flex-1 mb-6">
-        <CardHeader title="Redis Setting" />
-        <CardBody>
-          <div className="mb-4">
-            <label htmlFor="address" className="block text-sm font-medium text-gray-700">
-              Address
-            </label>
-            <input
-              type="text"
-              id="address"
-              name="address"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              className="w-full p-2 mt-1 border rounded-md"
-            />
-          </div>
+    {/* Card Redis Setting */}
+    <Card className="flex-1 mb-6">
+      <CardHeader title="Redis Setting" />
+      <CardBody>
+        <div className="flex justify-center">
+          <div className="w-1/2">
+            <div className="mb-4">
+              <label htmlFor="address" className="block text-sm font-medium text-gray-700">
+                Address
+              </label>
+              <input
+                type="text"
+                id="address"
+                name="address"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                className="block w-full p-2 mt-1 border rounded-md"
+              />
+            </div>
 
-          <div className="mb-4">
-            <label htmlFor="port" className="block text-sm font-medium text-gray-700">
-              Port
-            </label>
-            <input
-              type="text"
-              id="port"
-              name="port"
-              value={port}
-              onChange={(e) => setPort(e.target.value)}
-              className="w-full p-2 mt-1 border rounded-md"
-            />
-          </div>
+            <div className="mb-4">
+              <label htmlFor="port" className="block text-sm font-medium text-gray-700">
+                Port
+              </label>
+              <input
+                type="text"
+                id="port"
+                name="port"
+                value={port}
+                onChange={(e) => setPort(e.target.value)}
+                className="block w-full p-2 mt-1 border rounded-md"
+              />
+            </div>
 
-          <div className="mb-4">
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-2 mt-1 border rounded-md"
-            />
+            <div className="mb-4">
+              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="block w-full p-2 mt-1 border rounded-md"
+              />
+            </div>
           </div>
+        </div>
+
         </CardBody>
         <CardFooter>
           
@@ -61,38 +66,42 @@ const General = () => {
       </Card>
 
       <Card className="flex-1 mb-6">
-        <CardHeader title="Another Card" />
-        <CardBody>
-          <div className="flex mb-4">
-            <div className="flex-1 mr-2">
-              <label htmlFor="address2" className="block text-sm font-medium text-gray-700">
-                Address
-              </label>
-              <input
-                type="text"
-                id="address2"
-                name="address2"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-                className="w-full p-2 mt-1 border rounded-md"
-              />
-            </div>
-            <div className="flex-1 ml-2">
-              <label htmlFor="port2" className="block text-sm font-medium text-gray-700">
-                Port
-              </label>
-              <input
-                type="text"
-                id="port2"
-                name="port2"
-                value={port}
-                onChange={(e) => setPort(e.target.value)}
-                className="w-full p-2 mt-1 border rounded-md"
-              />
-            </div>
+  <CardHeader title="Another Card" />
+  <CardBody>
+    <div className="flex justify-center">
+      <div className="w-1/2">
+        <div className="flex mb-4">
+          <div className="flex-1 mr-2">
+            <label htmlFor="address2" className="block text-sm font-medium text-gray-700">
+              Address
+            </label>
+            <input
+              type="text"
+              id="address2"
+              name="address2"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              className="block w-full p-2 mt-1 border rounded-md"
+            />
           </div>
 
-          <div className="mb-4">
+          <div className="flex-1 ml-2">
+            <label htmlFor="port2" className="block text-sm font-medium text-gray-700">
+              Port
+            </label>
+            <input
+              type="text"
+              id="port2"
+              name="port2"
+              value={port}
+              onChange={(e) => setPort(e.target.value)}
+              className="block w-full p-2 mt-1 border rounded-md"
+            />
+          </div>
+        </div>
+
+        <div className="flex mb-4">
+          <div className="flex-1 mr-2">
             <label htmlFor="username" className="block text-sm font-medium text-gray-700">
               Username
             </label>
@@ -102,11 +111,10 @@ const General = () => {
               name="username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              className="w-full p-2 mt-1 border rounded-md"
+              className="block w-full p-2 mt-1 border rounded-md"
             />
           </div>
-
-          <div className="mb-4">
+          <div className="flex-1 ml-2">
             <label htmlFor="password" className="block text-sm font-medium text-gray-700">
               Password
             </label>
@@ -116,16 +124,19 @@ const General = () => {
               name="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-2 mt-1 border rounded-md"
+              className="block w-full p-2 mt-1 border rounded-md"
             />
           </div>
-        </CardBody>
-        <CardFooter>
-          {/**/}
-        </CardFooter>
-      </Card>
+        </div>
+      </div>
+    </div>
+  </CardBody>
+  <CardFooter>
+    {/**/}
+  </CardFooter>
+</Card>
 
-      {/* ... (Other cards) */}
+
     </div>
   );
 };
