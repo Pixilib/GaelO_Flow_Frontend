13ed474a12e896fb001e6a86851600bc737f006b
remixxx31 <remi.perray@epitech.eu>
2024-01-26 09:29:23 +0100

    style: :lipstick: Adapt components of Sidebar

--- src/RenderComponents/NavBar/MenuItemsCollapse.tsx
+++ src/RenderComponents/NavBar/MenuItemsCollapse.tsx
@@ -23,20 +23,21 @@ type MenuItemCollapseProps = {
     toggleOpen,
     onNavigate,
   }: MenuItemCollapseProps) => {
-    const bgIsOpen = isOpen ? "bg-inherit hover:bg-[#0C0B76] bg-inherit" : "";
+  //? Personnalize css if Menu is Open or not
+    const bgIsOpen = isOpen ? "bg-primary-hover" : "hover:bg-primary-hover";
     return (
       <div
-        className={`flex flex-col w-full ${className}`}
+        className={`flex flex-col w-full cursor-context-menu ${className}`}
         onClick={toggleOpen}
         data-gaelo-flow="sidebar-item-collapse"
       >
         <div className={`flex justify-between p-3 text-white ${bgIsOpen}`}>
           
           <div className={`flex items-center flex-grow ml-4`}>
-            <span className="mr-2">{icon}</span>
+            <span className="mr-3">{icon}</span>
             <span className="flex-grow">{title}</span>
           </div>
-          <span className="flex-shrink-0">{isOpen ? <ChevronUp /> : <ChevronDown />}</span>
+          <span className="flex-shrink-0 mr-4">{isOpen ? <ChevronUp /> : <ChevronDown />}</span>
           
         </div>
         {isOpen && <Items items={items} onNavigate={onNavigate} />}
