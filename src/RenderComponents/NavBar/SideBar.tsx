bdf78d14da76a00fc2d13ebc85e4a76e42e71214
remixxx31 <remi.perray@epitech.eu>
2024-01-26 08:27:24 +0100

    Update wip SideBar

--- src/RenderComponents/NavBar/SideBar.tsx
+++ src/RenderComponents/NavBar/SideBar.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
-import MenuItem  from "./MenuItem";
+import MenuItem from "./MenuItem";
 import LogoSideBar from "../../assets/LogoGaeloFlow-white3.svg?react";
-import Administrator from "../../assets/administrator_line_icon_236151 1 (1).svg?react";
+import Administrator from "../../assets/administrator.svg?react";
 import Search from "../../assets/search.svg?react";
 import MenuItemsCollapse from "./MenuItemsCollapse";
 
@@ -41,7 +41,7 @@ export const SideBar = () => {
   return (
     <nav
       data-gaelo-flow="sidebar"
-      className="flex flex-col w-full h-full shadow-custom"
+      className="flex flex-col w-full h-full shadow-custom rounded-tr-3xl bg-primary"
     >
       <div className="h-full justify-center rounded-tr-3xl top-0 start-0 bottom-0 z-[60] w-64 bg-primary pt-7 pb-10 overflow-y-hidden">
         <LogoSideBar className="mx-auto mb-40" />
@@ -51,22 +51,27 @@ export const SideBar = () => {
             title="Administration"
             items={authItems}
             isOpen={openItem === "Administration"}
-          toggleOpen={() => toggleOpen("Administration")}
+            toggleOpen={() => toggleOpen("Administration")}
             onNavigate={handleItemClick}
-          /> 
-        <MenuItem
-          title="Orthanc Content"
-          icon={<Search />}
-          isActive={location.pathname === "/"}
-          onClick={() => handleItemClick("/")}
-        />
- 
-        <MenuItem
-          title="Administration"
-          icon={<Administrator />}
-          isActive={location.pathname === "/lost-password"}
-          onClick={() => handleItemClick("/lost-password")}
-        />
+          />
+          <MenuItem
+            title="Orthanc Content"
+            icon={<Search />}
+            isActive={location.pathname === "/"}
+            onClick={() => handleItemClick("/")}
+          />
+          <MenuItem
+            title="Import"
+            icon={<Search />}
+            isActive={location.pathname === "/"}
+            onClick={() => handleItemClick("/")}
+          />
+          <MenuItem
+            title="Administration"
+            icon={<Administrator />}
+            isActive={location.pathname === "/lost-password"}
+            onClick={() => handleItemClick("/lost-password")}
+          />
         </section>
       </div>
     </nav>
