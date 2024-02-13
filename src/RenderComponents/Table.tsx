17bf5695b6e6f0171b29436a5fe02a4f1aa32722
Sophie Labyt <labytsophie@gmail.com>
2024-02-13 16:46:19 +0100

    💄 add padding content column, border and style font

--- src/RenderComponents/Table.tsx
+++ src/RenderComponents/Table.tsx
@@ -1,16 +1,12 @@
-import {
-    flexRender,
-    getCoreRowModel,
-    useReactTable,
-} from '@tanstack/react-table';
+import React from 'react';
+import { useReactTable, getCoreRowModel, flexRender } from '@tanstack/react-table';
 
 type TableProps = {
     data: Object[],
     columns: Array<any>
-}
+};
 
 const Table = ({ data, columns }: TableProps) => {
-
     const table = useReactTable({
         data,
         columns,
@@ -18,53 +14,33 @@ const Table = ({ data, columns }: TableProps) => {
     });
 
     return (
-        <table>
-            <thead>
-                {table.getHeaderGroups().map(headerGroup => (
-                    <tr key={headerGroup.id}>
-                        {headerGroup.headers.map(header => (
-                            <th key={header.id}>
-                                {header.isPlaceholder
-                                    ? null
-                                    : flexRender(
-                                        header.column.columnDef.header,
-                                        header.getContext()
-                                    )}
-                            </th>
-                        ))}
-                    </tr>
-                ))}
-            </thead>
-            <tbody>
-                {table.getRowModel().rows.map(row => (
-                    <tr key={row.id}>
-                        {row.getVisibleCells().map(cell => (
-                            <td key={cell.id}>
-                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                            </td>
-                        ))}
-                    </tr>
-                ))}
-            </tbody>
-            <tfoot>
-                {table.getFooterGroups().map(footerGroup => (
-                    <tr key={footerGroup.id}>
-                        {footerGroup.headers.map(header => (
-                            <th key={header.id}>
-                                {header.isPlaceholder
-                                    ? null
-                                    : flexRender(
-                                        header.column.columnDef.footer,
-                                        header.getContext()
-                                    )}
-                            </th>
-                        ))}
-                    </tr>
-                ))}
-            </tfoot>
-        </table>
+        <div className="overflow-x-auto">
+            <table className="min-w-full bg-white rounded-lg">
+                <thead className="border-b-2 border-gray-300">
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map(header => (
+                                <th key={header.id} className="px-2 py-3 text-left text-xs font-bold text-bg-gray uppercase tracking-wider md:px-4 lg:px-6 min-w-[120px] text-lg border-b-2 border-gray">
+                                    {flexRender(header.column.columnDef.header, header.getContext())}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody>
+                    {table.getRowModel().rows.map(row => (
+                        <tr key={row.id}>
+                            {row.getVisibleCells().map(cell => (
+                                <td key={cell.id} className="px-2 py-4 whitespace-nowrap md:px-4 lg:px-6 min-w-[120px]">
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
-export default Table
-
+export default Table;
--- src/admin/general/RedisCard.tsx
+++ src/admin/general/RedisCard.tsx
@@ -1,62 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createColumnHelper } from "@tanstack/react-table";
-
 import Card, { CardHeader, CardBody, CardFooter } from '../../RenderComponents/Card';
 import Table from '../../RenderComponents/Table';
 
 interface RedisData {
-  address: string;
-  port: number;
-  password: string;
+    address: string;
+    port: number;
+    password: string;
 }
 
 const RedisCard: React.FC = () => {
-  const columnHelper = createColumnHelper<RedisData>();
-  const data = [
-    {
-      address: 'https://orthanc.fr',
-      port: 8042,
-      password: 'password',
-    },
-    {
-      address: 'https://orthanc2.fr',
-      port: 8043,
-      password: 'password',
-    },
-  ];
+    const columnHelper = createColumnHelper<RedisData>();
+    const [data, setData] = useState<RedisData[]>([]); 
+    useEffect(() => {
+       
+        const fetchData = async () => {
+          
+            const loadedData = [
+                {
+                    address: 'https://orthanc',
+                    port: 1234,
+                    password: 'password',
+                },
+                
+            ];
+            setData(loadedData); 
+        };
 
-  const columns = [
-    columnHelper.accessor('address', {
-      header: 'Address',
-      cell: info => info.getValue(),
-    }),
-    columnHelper.accessor('port', {
-      header: 'Port',
-      cell: info => info.getValue(),
-    }),
-    columnHelper.accessor('password', {
-      header: 'Password',
-      cell: () => '••••••',
-    }),
-  ];
+        fetchData();
+    }, []); 
+    const columns = [
+        columnHelper.accessor('address', {
+            header: 'Address',
+            cell: info => info.getValue(),
+        }),
+        columnHelper.accessor('port', {
+            header: 'Port',
+            cell: info => info.getValue(),
+        }),
+        columnHelper.accessor('password', {
+            header: 'Password',
+            cell: () => '••••••',
+        }),
+    ];
 
-  return (
-    <div className='mt-4'>
-      <Card>
-        <CardHeader title="Redis Settings" />
-        <CardBody>
-          <div className="flex justify-center">
-            <div className="w-full mb-4">
-              <div className="min-w-full overflow-hidden bg-white sm:rounded-lg">
-                <Table columns={columns} data={data} />
-              </div>
-            </div>
-          </div>
-        </CardBody>
-        <CardFooter />
-      </Card>
-    </div>
-  );
+    return (
+        <div className='mt-4'>
+            <Card>
+                <CardHeader title="Redis Settings" />
+                <CardBody>
+                    <div className="flex justify-center">
+                        <div className="w-full mb-4">
+                            <Table columns={columns} data={data} />
+                        </div>
+                    </div>
+                </CardBody>
+                <CardFooter />
+            </Card>
+        </div>
+    );
 };
 
 export default RedisCard;
