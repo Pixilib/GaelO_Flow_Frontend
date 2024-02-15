8236f96ab14e622752107baba76a585d0ab85a49
Sophie Labyt <labytsophie@gmail.com>
2024-02-15 17:08:53 +0100

    style column

--- src/RenderComponents/Table.tsx
+++ src/RenderComponents/Table.tsx
@@ -19,11 +19,15 @@ const Table = ({ data, columns }: TableProps) => {
                 <thead className="border-b-2 border-gray-200">
                     {table.getHeaderGroups().map(headerGroup => (
                         <tr key={headerGroup.id}>
-                            {headerGroup.headers.map(header => (
-                                <th key={header.id} className="px-2 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider md:px-4 lg:px-6 min-w-[120px] text-lg border-b-2 border-gray">
-                                    {flexRender(header.column.columnDef.header, header.getContext())}
-                                </th>
-                            ))}
+                            {headerGroup.headers.map(header => {
+                                // Applique un centrage pour les colonnes spécifiées
+                                const textAlignClass = ['address', 'port', 'password'].includes(header.id) ? 'text-center' : 'text-left';
+                                return (
+                                    <th key={header.id} className={`px-2 py-3 ${textAlignClass} text-xs font-bold text-dark uppercase tracking-wider md:px-4 lg:px-6 min-w-[120px] text-lg border-b-2 border-gray`}>
+                                        {flexRender(header.column.columnDef.header, header.getContext())}
+                                    </th>
+                                );
+                            })}
                         </tr>
                     ))}
                 </thead>
@@ -31,15 +35,16 @@ const Table = ({ data, columns }: TableProps) => {
                     {table.getRowModel().rows.map(row => (
                         <tr key={row.id}>
                             {row.getVisibleCells().map(cell => {
-                                const isPortColumn = cell.column.id === 'port';
+                                const isCenteredColumn = ['address', 'port', 'password'].includes(cell.column.id);
+                                const textAlignClass = isCenteredColumn ? 'text-center' : 'text-left';
+                                const cellContent = flexRender(cell.column.columnDef.cell, cell.getContext());
                                 return (
-                                    <td key={cell.id} className="px-2 py-4 whitespace-nowrap md:px-4 lg:px-6 min-w-[120px]">
-                                        {isPortColumn ? (
-                                            <span className="w-12 h-[27px] px-3 py-2.5 bg-green-200 text-dark rounded-lg flex justify-center items-center gap-2">
-                                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    <td key={cell.id} className={`px-2 py-4 whitespace-nowrap md:px-4 lg:px-6 min-w-[120px] ${textAlignClass}`}>
+                                        {cell.column.id === 'port' ? (
+                                            <span className="inline-flex items-center w-12 h-[27px] px-3 py-2.5 bg-green-200 text-dark rounded-lg  justify-center items-center gap-2">                                                {cellContent}
                                             </span>
                                         ) : (
-                                            flexRender(cell.column.columnDef.cell, cell.getContext())
+                                            cellContent
                                         )}
                                     </td>
                                 );
