ae0c63c97f29bfb7671601635664035614991dbe
Sophie Labyt <labytsophie@gmail.com>
2024-02-27 15:29:16 +0100

    🎨 improve data handling with 'select' in Orthanc API query

--- src/admin/general/OrthancCard.tsx
+++ src/admin/general/OrthancCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ColumnDef } from '@tanstack/react-table';
-import { useCustomQuery } from "../../utils/reactQuery";
-import { apiSystem } from '../../services/orthanc';
+import { useCustomQuery } from '../../utils/reactQuery';
+import { getOrthancSystem } from '../../services/orthanc';
 
 import Card, { CardHeader, CardBody, CardFooter } from '../../RenderComponents/Card';
 import Table from '../../RenderComponents/Table';
@@ -11,8 +11,6 @@ import { Colors } from '../../utils/enums';
 import Restart from '../../assets/restart.svg?react';
 import Shutdown from '../../assets/shutdown.svg?react';
 import Info from '../../assets/info.svg?react';
-import { useCustomQuery } from '../../utils/reactQuery';
-import { getOrthancSystem } from '../../services/orthanc';
 
 //!Maybe needs to change interface see src/types/Orthanc/OrthancSystemType.ts
 //WIP
@@ -30,16 +28,19 @@ const Badge: React.FC<{ value: number }> = ({ value }) => {
     return <span className={badgeClasses}>{value}</span>;
 };
 
+
 const OrthancSettingsCard: React.FC = () => {
-    const { data: orthancData, error, isPending } = useCustomQuery<OrthancData[]>('orthancSystem', getOrthancSystem);  
-    
-    //!It's for exemple, you can change the return of the request
-    if (isPending) return <span>Loading ... </span>
-    if (error) return <span>Error: {error.message}</span>
-    if (!orthancData) return null
-    //! Just for visualize example of response
-    if (orthancData)
-    console.log({ orthancData })
+    const { data: orthancData, error, isPending } = useCustomQuery<OrthancData[]>('orthancSystem', getOrthancSystem, {
+        select: (data: OrthancData[]) => data.map((item: OrthancData) => ({
+            ...item,
+            port: Number(item.port), 
+        })),
+    });
+
+    console.log("Orthanc Data:", orthancData);
+    if (isPending) return <span>Loading ...</span>;
+    if (error) return <span>Error: {error.message}</span>;
+    if (!orthancData) return null;
 
     const columns: ColumnDef<OrthancData>[] = [
         {
@@ -66,10 +67,6 @@ const OrthancSettingsCard: React.FC = () => {
 
     return (
         <div className='mt-4'>
-            <div>
-                <h1>Orthanc Data</h1>
-                <pre>{JSON.stringify(orthancData, null, 2)}</pre>
-            </div>
             <Card>
                 <CardHeader title="Orthanc Settings" />
                 <CardBody>
@@ -80,14 +77,13 @@ const OrthancSettingsCard: React.FC = () => {
                     </div>
                 </CardBody>
                 <CardFooter className="flex justify-center space-x-4">
-                    <Button color={Colors.orange} onClick={() => console.log('Restart action')}><Restart /></Button>
-                    <Button color={Colors.danger} onClick={() => console.log('Shutdown action')}><Shutdown /></Button>
-                    <Button color={Colors.primary} onClick={() => console.log('Info action')}><Info /></Button>
+                    <Button color={Colors.orange} onClick={() => console.log('Restart action')}><img src={Restart} alt="Restart" /></Button>
+                    <Button color={Colors.danger} onClick={() => console.log('Shutdown action')}><img src={Shutdown} alt="Shutdown" /></Button>
+                    <Button color={Colors.primary} onClick={() => console.log('Info action')}><img src={Info} alt="Info" /></Button>
                 </CardFooter>
             </Card>
         </div>
     );
 };
-
 
 export default OrthancSettingsCard;
