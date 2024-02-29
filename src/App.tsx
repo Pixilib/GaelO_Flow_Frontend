d4eb5e4947486a6b7f546c222932d17faf68f44b
Sophie Labyt <labytsophie@gmail.com>
2024-02-29 11:19:20 +0100

    correction app

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import WelcomeRoot from "./welcome/WelcomeRoot";
 import { RootState } from "./store";
 import RootApp from "./root/RootApp";
 import "./index.css";
-import OrthancSettingsCard from "./admin/general/OrthancCard";
+
+
 function App() {
   const isLogged = useSelector((state: RootState) => state.user.isLogged);
-  return(
-    <OrthancSettingsCard/>
-  )
- 
+  
   return (
   <div className="w-screen h-screen">
     {isLogged ? <RootApp /> : <WelcomeRoot />}
