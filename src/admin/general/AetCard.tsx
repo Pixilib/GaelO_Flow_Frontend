9f3a149113c095a92ed66977a9f9f8d193b63d3e
Sophie Labyt <labytsophie@gmail.com>
2024-03-19 16:57:16 +0100

    Add button New Aet

--- src/admin/general/AetCard.tsx
+++ src/admin/general/AetCard.tsx
@@ -1,60 +1,70 @@
 import React from 'react';
 import Card, { CardHeader, CardBody } from '../../ui/Card';
-import Table from '../../ui/table/Table'
+import Table from '../../ui/table/Table';
 import Badge from '../../ui/Badge';
 import { ColumnDef } from '@tanstack/react-table';
-
+import Button from '../../ui/Button';
+import { AiOutlinePlus as MoreIcon } from "react-icons/ai";
 interface AetData {
-  // Définissez ici les propriétés de votre objet AetData
-  name: string;
-  Aet: number;
-  Host: string;
-  Manufacturer: string;
-  // Ajoutez les autres champs nécessaires
+    name: string;
+    Aet: number;
+    Host: string;
+    Manufacturer: string;
 }
 
 interface AetProps {
-  aetData: AetData[];
+    aetData: AetData[];
 }
 
 const Aet: React.FC<AetProps> = ({ aetData }) => {
-  const columns: ColumnDef<AetData>[] = [
-    {
-      accessorKey: 'name',
-      header: 'NAME',
-    },
-    {
-      accessorKey: 'Aet',
-      header: 'AET',
-      cell: (row: any) => <Badge value={row.getValue() as number} />,
-    },
-    {
-      accessorKey: 'Host',
-      header: 'host',
-    },
-    {
-        accessorKey: 'Manufacturer',
-        header: 'MANUFACTURER',
-      },  {
-        accessorKey: 'EchoAet',
-        header: 'ECHO AET',
-      }, {
-        accessorKey: 'Remove Aet',
-        header: 'REMOVE AET',
-      },  ];
+    const columns: ColumnDef<AetData>[] = [
+        {
+            accessorKey: 'name',
+            header: 'NAME',
+        },
+        {
+            accessorKey: 'Aet',
+            header: 'AET',
+            cell: (info) => <Badge value={info.getValue() as number} />,
+        },
+        {
+            accessorKey: 'Host',
+            header: 'HOST',
+        },
+        {
+            accessorKey: 'Manufacturer',
+            header: 'MANUFACTURER',
+        },
+        {
+            accessorKey: 'EchoAet',
+            header: 'ECHO AET',
+        },
+        {
+            accessorKey: 'RemoveAet',
+            header: 'REMOVE AET',
+        },
+    ];
 
-  return (
-    <Card>
-      <CardHeader title="New AET" />
-      <CardBody>
-        <div className="flex justify-center">
-          <div className="w-full mb-4">
-            <Table columns={columns} data={aetData} />
-          </div>
-        </div>
-      </CardBody>
-    </Card>
-  );
+    return (
+        <Card>
+            <CardHeader title="AETS" />
+            <CardBody>
+                <div className="flex flex-col items-center">
+                    <div className="w-full mb-4">
+                        <Table columns={columns} data={aetData} />
+                    </div>
+                    <div className="flex justify-center w-full mt-2">
+                        <Button color="success">
+                            <span className="flex items-center mr-2">
+                                <MoreIcon className="mr-2" size={24} />
+                                New AET
+                            </span>
+                        </Button>
+                    </div>
+                </div>
+            </CardBody>
+        </Card>
+    );
 };
 
 export default Aet;
