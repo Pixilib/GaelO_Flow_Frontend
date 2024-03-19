3ee6bac1401b6489915b3a156f2be2c6f8caf262
Sophie Labyt <labytsophie@gmail.com>
2024-03-19 10:03:36 +0100

    Style : SelectInput  correction tailwindcss

--- src/ui/SelectInput.tsx
+++ src/ui/SelectInput.tsx
@@ -19,20 +19,20 @@ interface SelectionInputProps {
 const customStyles: StylesConfig<OptionType, boolean> = {
   control: (provided, state) => ({
     ...provided,
-    borderRadius: '15px',
-    borderColor: state.isFocused ? '#333182' : '#E2E8F0',
+    borderRadius: '0.375rem', 
+    borderColor: state.isFocused ? 'indigo-600' : 'gray-200', 
     boxShadow: state.isFocused ? '0 0 0 1px #4F46E5' : 'none',
     '&:hover': {
-      borderColor: state.isFocused ? '#333182' : 'Primary',
+      borderColor: state.isFocused ? 'indigo-600' : 'primary', 
     },
-    backgroundColor: state.isFocused ? '#EFF6FF' : 'EFF6FF',
+    backgroundColor: state.isFocused ? '#EFF6FF' : '#EFF6FF',
   }),
   menu: (provided) => ({
     ...provided,
-    borderRadius: '15px',
-    borderColor: '#E2E8F0',
+    borderRadius: '0.375rem',
+    borderColor: 'gray-200', 
   }),
-
+  
 };
 
 const SelectionInput: React.FC<SelectionInputProps> = ({
