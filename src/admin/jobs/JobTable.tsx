e07c8f11edb4f176b717a0fd507d3648be9f3382
remixxx31 <remi.perray@epitech.eu>
2024-03-20 11:04:55 +0100

    ✨ popoverin JobTable

--- src/admin/jobs/JobTable.tsx
+++ src/admin/jobs/JobTable.tsx
@@ -4,6 +4,7 @@ import Badge from "../../ui/Badge";
 
 import { Colors } from "../../utils/enums";
 import JobIcons from "./JobIcons";
+import Popover from "../../ui/Popover";
 
 //WIP for actions
 const JobTable = ({ data = [] }) => {
@@ -17,21 +18,21 @@ const JobTable = ({ data = [] }) => {
       header: "Progress",
       cell: (row: any) => (
         <Badge
-          value={row.getValue()}
-          className="rounded-full bg-badge-grayCustom text-badge-blue-text"
+        value={row.getValue()}
+        className="rounded-full bg-badge-grayCustom text-badge-blue-text"
         />
-      ),
-    },
-    {
-      accessorKey: "State",
-      header: "State",
-      cell: (row: any) => <Badge value={row.getValue()} className="rounded-full bg-[#CDFFCD] text-success" />,
-    },
-    {
-      header: "Action",
-      cell: () => {
-        return (
-          <div className="flex justify-center">
+        ),
+      },
+      {
+        accessorKey: "State",
+        header: "State",
+        cell: (row: any) => <Badge value={row.getValue()} className="rounded-full bg-[#CDFFCD] text-success" />,
+      },
+      {
+        header: "Action",
+        cell: () => {
+          return (
+            <div className="flex justify-center">
             <JobIcons/>   
           </div>
         );
@@ -40,17 +41,24 @@ const JobTable = ({ data = [] }) => {
     },
     {
       header: "Info",
-      cell: () => {
+      cell: (info:any) => {
+        const row = info.row.original;
         return (
+          <Popover popover={infoDetails(row)} placement="left" className="w-auto ">
           <div className="flex justify-center transition-transform hover:scale-110">
-            <BsInfoCircle size="2em" color="gray"  />
+            <BsInfoCircle size="1.5em" color="gray" onClick={() => handleInfoClick(row)}/>
           </div>
+          </Popover>
         );
       },
       enableColumnFilter: false,
     }
   ];
-
+  const infoDetails = (rowData:any) => <pre className="text-xs">{JSON.stringify(rowData, null, 4)}</pre>;
+  const handleInfoClick = (rowData:any) => {
+    console.log('Row data:', JSON.stringify(rowData, null, 2));
+    return <pre>{JSON.stringify(rowData, null, 4)}</pre>;
+  };
   return <Table data={data} columns={columns} color={Colors.almond} />;
 };
 export default JobTable;
