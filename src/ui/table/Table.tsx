a662d864a06d3eda6d4cbca266031a6c266222fa
Sophie Labyt <labytsophie@gmail.com>
2024-03-28 13:14:40 +0100

    Style: props headercolor for table component

--- src/ui/table/Table.tsx
+++ src/ui/table/Table.tsx
@@ -11,18 +11,19 @@ import {
   getFilteredRowModel,
   getPaginationRowModel,
 } from '@tanstack/react-table';
-import FilterTable from './FilterTable'; 
+import FilterTable from './FilterTable';
 import Footer from '../table/Footer';
 
 type TableProps<TData> = {
   data: TData[];
   columns: ColumnDef<TData>[];
   enableSorting?: boolean;
-  color?: Colors;
+  headerColor: Colors;
   className?: string;
 };
 //WIP : Pagination
-function Table<T>({ data, columns, enableSorting = true, color, className }: TableProps<T>) {
+function Table<T>({ data, columns, enableSorting = true, headerColor, className }: TableProps<T>) {
+
   const [sorting, setSorting] = useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
   const [pagination, setPagination] = useState({
@@ -49,15 +50,16 @@ function Table<T>({ data, columns, enableSorting = true, color, className }: Tab
     enableColumnFilters: true,
     enableSorting,
   });
+  const headerBgClass = `bg-${headerColor}`;
 
   return (
-    <div className="max-h-[500px] overflow-x-auto rounded-xl">
+    <div className={`max-h-[500px] overflow-x-auto rounded-xl ${className}`}>
       <table className={`min-w-full border-grayCustom ${className}`}>
-        <thead className={`border-grayCustom  bg-${color}`}>
+        <thead className={headerBgClass}>
           {table.getHeaderGroups().map(headerGroup => (
             <>
               {/* Ligne pour les titres et les flèches de tri */}
-              <tr key={headerGroup.id} className={`bg-${color}`}>
+              <tr key={headerGroup.id} className={headerBgClass}>
                 {headerGroup.headers.map(header => (
                   <th
                     key={header.id}
