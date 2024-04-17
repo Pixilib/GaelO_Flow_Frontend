2e6d5185bdfc66b86d71745e3e8c70b51a0b0544
remixxx31 <remi.perray@epitech.eu>
2024-04-17 16:14:52 +0200

    style(Tabs date): Update style Tabs, refacto date

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -12,6 +12,31 @@ export const formatTime = (hours: number, minutes: number): string => {
 }
 
 /**
+ * Parses a time string into an object with hour and minute properties.
+ * @param {string} timeString - The time string to parse, e.g., "12:30".
+ * @returns {{hours: number, minutes: number}} An object with hour and minute properties.
+ * @example
+ * * const timeObj = strToHrMin("12:30");
+ * * console.log(timeObj); // {hours: 12, minutes: 30}
+ */
+export const parseTimeString = (timeString: string): {hours: number, minutes: number} => {
+    const [hours, minutes] = timeString.split(':').map(part => parseInt(part, 10));
+    return {hours, minutes};
+}
+
+/**
+ * Formats a time string for display in a human-readable format specifying hours and minutes.
+ * @param {string} time - Time string in HH:MM format, e.g., "12:30".
+ * @returns {string} A descriptive time format, e.g., "12 hours 30 minutes".
+ */
+export const formatTimeReadable = (time: string): string => {
+    const [hours, minutes] = time.split(':').map(Number);
+    const hourLabel = `${hours} hour${hours > 1 ? 's' : ''}`;
+    const minuteLabel = `${minutes} minute${minutes > 1 ? 's' : ''}`;
+    return `${hourLabel} ${minutes > 0 ? minuteLabel : ''}`.trim();
+}
+
+/**
  * Calculate the difference between two time strings in hours and minutes
  * @param {string} timeStart example 12:30
  * @param {string} timeEnd example 14:00
@@ -29,27 +54,3 @@ export const timeDiff = (timeStart: string, timeEnd: string): string => {
     return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
 }
 
-/**
- * Formats a time string for display in a human-readable format specifying hours and minutes.
- * @param {string} time - Time string in HH:MM format, e.g., "12:30".
- * @returns {string} A descriptive time format, e.g., "12 hours 30 minutes".
- */
-export const formatTimeReadable = (time: string): string => {
-    const [hours, minutes] = time.split(':').map(Number);
-    const hourLabel = `${hours} hour${hours > 1 ? 's' : ''}`;
-    const minuteLabel = `${minutes} minute${minutes > 1 ? 's' : ''}`;
-    return `${hourLabel} ${minutes > 0 ? minuteLabel : ''}`.trim();
-}
-
-/**
- * Parses a time string into an object with hour and minute properties.
- * @param {string} timeString - The time string to parse, e.g., "12:30".
- * @returns {{hours: number, minutes: number}} An object with hour and minute properties.
- * @example
- * * const timeObj = strToHrMin("12:30");
- * * console.log(timeObj); // {hours: 12, minutes: 30}
- */
-export const parseTimeString = (timeString: string): {hours: number, minutes: number} => {
-    const [hours, minutes] = timeString.split(':').map(part => parseInt(part, 10));
-    return {hours, minutes};
-}
