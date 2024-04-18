fb2e1dde251bce339339d5ba1a34643365a3f466
Sophie Labyt <labytsophie@gmail.com>
2024-04-18 16:54:20 +0200

    correction shadow

--- src/ui/table/Footer.tsx
+++ src/ui/table/Footer.tsx
@@ -5,7 +5,6 @@ import { Table } from "@tanstack/react-table";
 type FooterProps = {
   table: Table<any>;
 }
-
 const Footer = ({ table }: FooterProps) => {
   const {
     getState,
@@ -17,36 +16,53 @@ const Footer = ({ table }: FooterProps) => {
   } = table;
   const { pageIndex } = getState().pagination;
   const totalRows = getPrePaginationRowModel().rows.length;
-  const btnClassFooter = "bg-transparent p-2 focus:outline-none text-dark";
-
-  const shadowStyle = {
-    boxShadow: '0 -8px 10px -5px rgba(0, 0, 0, 0.2), 0 8px 10px -5px rgba(0, 0, 0, 0.2)'
-  };
 
   return (
-    <div className={`flex justify-end w-full rounded bg-white`} style={shadowStyle}>
-      <div className="flex items-center justify-end flex-grow me-4">
-        <strong className="me-4">{totalRows}</strong>
-        <em>{totalRows > 1 ? "results" : "result"}</em>
-      </div>
-      <div className="flex">
-        <button className={btnClassFooter} onClick={() => setPageIndex(0)} disabled={!getCanPreviousPage} aria-label="Go to the first page!">
-          <MdSkipPrevious size="2em" />
-        </button>
-        <button className={btnClassFooter} onClick={() => setPageIndex(Math.max(pageIndex - 1, 0))} disabled={!getCanPreviousPage} aria-label="Go to the previous page!">
-          <GrPrevious size="1.3em" />
-        </button>
+    <div className="flex justify-end w-full bg-white border-t-2 border-gray-200 rounded shadow-md">
+      <div className="flex items-center justify-between flex-grow px-4 py-2">
+        <div className="text-left">
+          <strong>{totalRows}</strong>
+          <em>{totalRows > 1 ? " results" : " result"}</em>
+        </div>
+        <div className="flex">
+          <button 
+            className="p-2 bg-transparent focus:outline-none text-dark"
+            onClick={() => setPageIndex(0)} 
+            disabled={!getCanPreviousPage}
+            aria-label="Go to the first page!"
+          >
+            <MdSkipPrevious size="2em" />
+          </button>
+          <button 
+            className="p-2 bg-transparent focus:outline-none text-dark"
+            onClick={() => setPageIndex(Math.max(pageIndex - 1, 0))} 
+            disabled={!getCanPreviousPage}
+            aria-label="Go to the previous page!"
+          >
+            <GrPrevious size="1.3em" />
+          </button>
 
-        <span className="flex items-center">
-          <em className="me-1">Page</em> <strong> {pageIndex + 1} of {getPageCount()}</strong>
-        </span>
+          <span className="flex items-center px-3">
+            <em>Page</em> <strong> {pageIndex + 1} of {getPageCount()}</strong>
+          </span>
 
-        <button className={btnClassFooter} onClick={() => setPageIndex(Math.min(pageIndex + 1, getPageCount() - 1))} disabled={!getCanNextPage} aria-label="Go to the next page!">
-          <GrNext size="1.3em" />
-        </button>
-        <button className={btnClassFooter} onClick={() => setPageIndex(getPageCount() - 1)} disabled={!getCanNextPage} aria-label="Go to the last page">
-          <MdSkipNext size="2em" />
-        </button>
+          <button 
+            className="p-2 bg-transparent focus:outline-none text-dark"
+            onClick={() => setPageIndex(Math.min(pageIndex + 1, getPageCount() - 1))} 
+            disabled={!getCanNextPage}
+            aria-label="Go to the next page!"
+          >
+            <GrNext size="1.3em" />
+          </button>
+          <button 
+            className="p-2 bg-transparent focus:outline-none text-dark"
+            onClick={() => setPageIndex(getPageCount() - 1)} 
+            disabled={!getCanNextPage}
+            aria-label="Go to the last page"
+          >
+            <MdSkipNext size="2em" />
+          </button>
+        </div>
       </div>
     </div>
   );
