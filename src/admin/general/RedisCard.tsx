80eb8849b9abc28acb25b69ba1713c887ad58d77
Sophie Labyt <labytsophie@gmail.com>
2024-04-22 15:51:14 +0200

    wip

--- src/admin/general/RedisCard.tsx
+++ src/admin/general/RedisCard.tsx
@@ -5,15 +5,7 @@ import Table from '../../ui/table/Table';
 import Input from '../../ui/Input';
 import { Colors } from '../../utils/enums';
 import { ColumnDef } from '@tanstack/react-table';
-
-interface BadgeProps {
-    value: number;
-}
-
-const Badge: React.FC<BadgeProps> = ({ value }) => {
-    const badgeClasses = `rounded-xl bg-indigo-100 px-2 py-1 text-xs font-medium text-primary ring-1 ring-inset ring-green-600/20`;
-    return <span className={badgeClasses}>{value}</span>;
-};
+import { Badge } from '../../ui';
 
 interface RedisData {
     address: string;
@@ -36,12 +28,7 @@ const RedisCard: React.FC<RedisCardProps> = ({ redisData }) => {
             accessorKey: 'port',
             header: () => 'Port',
             cell: ({ getValue }) => <Badge value={getValue() as number} />,
-        },
-        {
-            accessorKey: 'password',
-            header: () => 'Password',
-            cell: ({ getValue }) => <Input disabled type='password' value={getValue() as string} />,
-        },
+        }
     ];
 
     return (
--- src/ui/table/Footer.tsx
+++ src/ui/table/Footer.tsx
@@ -18,15 +18,14 @@ const Footer = ({ table }: FooterProps) => {
   const totalRows = getPrePaginationRowModel().rows.length;
 
   return (
-    <div className="flex justify-end w-full bg-white border-t-2 border-gray-200 rounded shadow-md">
-      <div className="flex items-center justify-between flex-grow px-4 py-2">
-        <div className="text-left">
+    <div className="flex items-center justify-between flex-grow bg-white border-t-2 border-gray-200 rounded shadow-lg">
+        <div>
           <strong>{totalRows}</strong>
           <em>{totalRows > 1 ? " results" : " result"}</em>
         </div>
-        <div className="flex">
+        <div className="flex space-x-2">
           <button 
-            className="p-2 bg-transparent focus:outline-none text-dark"
+            className="bg-transparent focus:outline-none text-dark"
             onClick={() => setPageIndex(0)} 
             disabled={!getCanPreviousPage}
             aria-label="Go to the first page!"
@@ -34,7 +33,7 @@ const Footer = ({ table }: FooterProps) => {
             <MdSkipPrevious size="2em" />
           </button>
           <button 
-            className="p-2 bg-transparent focus:outline-none text-dark"
+            className="bg-transparent focus:outline-none text-dark"
             onClick={() => setPageIndex(Math.max(pageIndex - 1, 0))} 
             disabled={!getCanPreviousPage}
             aria-label="Go to the previous page!"
@@ -42,12 +41,12 @@ const Footer = ({ table }: FooterProps) => {
             <GrPrevious size="1.3em" />
           </button>
 
-          <span className="flex items-center px-3">
+          <span>
             <em>Page</em> <strong> {pageIndex + 1} of {getPageCount()}</strong>
           </span>
 
           <button 
-            className="p-2 bg-transparent focus:outline-none text-dark"
+            className="bg-transparent focus:outline-none text-dark"
             onClick={() => setPageIndex(Math.min(pageIndex + 1, getPageCount() - 1))} 
             disabled={!getCanNextPage}
             aria-label="Go to the next page!"
@@ -55,7 +54,7 @@ const Footer = ({ table }: FooterProps) => {
             <GrNext size="1.3em" />
           </button>
           <button 
-            className="p-2 bg-transparent focus:outline-none text-dark"
+            className="bg-transparent focus:outline-none text-dark"
             onClick={() => setPageIndex(getPageCount() - 1)} 
             disabled={!getCanNextPage}
             aria-label="Go to the last page"
@@ -63,7 +62,6 @@ const Footer = ({ table }: FooterProps) => {
             <MdSkipNext size="2em" />
           </button>
         </div>
-      </div>
     </div>
   );
 };
