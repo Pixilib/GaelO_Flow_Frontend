15d3380f726151627eb892b0f1e4a5030f9b43ee
Sophie Labyt <labytsophie@gmail.com>
2024-05-29 14:04:30 +0200

    style : add icon input label form

--- src/admin/Labels/LabelInputForm.tsx
+++ src/admin/Labels/LabelInputForm.tsx
@@ -22,22 +22,20 @@ const LabelInputForm: React.FC<LabelInputFormProps> = ({ onCreate }) => {
     };
 
     return (
-        <div className="flex items-center">
-            <div className="relative flex items-center w-full">
-                <MdOutlineNewLabe />
-                <Input
-                    type="text"
-                    value={label}
-                    onChange={handleInputChange}
-                    placeholder="Add new label"
-                    className="w-full px-4 py-4 border border-gray-300 rounded-r-none rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-            </div>
+        <div className="relative flex items-center">
+            <Input
+                type="text"
+                value={label}
+                onChange={handleInputChange}
+                placeholder="Add new label"
+                className="w-full px-12 py-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            <MdOutlineNewLabel className="absolute text-gray-400 transform -translate-y-1/2 top-1/2 left-3" />
             <Button
                 type="button"
                 color={Colors.success}
                 onClick={handleCreateClick}
-                className="p-3 text-white rounded-l-none rounded-r-md"
+                className="absolute right-0 p-3 text-white transform -translate-x-full -translate-y-1/2 rounded-l-none top-1/2 rounded-r-md"
             >
                 Create
             </Button>
