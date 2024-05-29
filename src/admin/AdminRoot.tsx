e65bdcb76e06c7cb8966afbb372ea0157b1d8d38
remixxx31 <remi.perray@epitech.eu>
2024-05-29 13:42:12 +0200

    refactor: Remove unused LabelsRoot component and related files

--- src/admin/AdminRoot.tsx
+++ src/admin/AdminRoot.tsx
@@ -6,7 +6,6 @@ import QueuesRoot from "./queues/QueuesRoot";
 import ModalitiesRoot from "./modalities/ModalitiesRoot";
 import UsersRoot from "./users/UsersRoot";
 import PeersRoot from "./peers/PeersRoot";
-import LabelsRoot from "./labels/LabelsRoot";
 
 const AdminRoot = () => {
     return (
@@ -17,7 +16,6 @@ const AdminRoot = () => {
                 <Route path="/modalities" element={<ModalitiesRoot />} />
                 <Route path="/queues/*" element={<QueuesRoot />} />
                 <Route path="/users/*" element={<UsersRoot />} />
-                <Route path="/labels/*" element={<LabelsRoot />} />
                 <Route path="/peers" element={<PeersRoot/>} />
             </Routes>
         </div>
