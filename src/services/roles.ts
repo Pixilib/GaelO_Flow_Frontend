d7cb1f59b4c9400ff0d83fabe6534b7cbbad24b7
remixxx31 <remi.perray@epitech.eu>
2024-06-13 10:36:15 +0200

    merge form dev

--- src/services/roles.ts
+++ src/services/roles.ts
@@ -1,49 +1,25 @@
-//import axios from './axios';
+import axios from './axios';
 
-// _export const getLabels = (): Promise<Label[]> => {
-//     return axios
-//         .get(`/api/labels`)
-//         .then(response => response.data)
-//         .catch(error => {
-//             if (error.response) {
-//                 throw error.response;
-//             }
-//             throw error;
-//         });
-// };
+export const getRoles = (): Promise<Roles[]> => {
+    return axios
+        .get(`/api/roles`)
+        .then(response => response.data)
+        .catch(error => {
+            if (error.response) {
+                throw error.response;
+            }
+            throw error;
+        });
+};
 
-// export const addLabel = (payload: Label): Promise<void> => { 
-//     return axios
-//         .post(`/api/labels`, payload)
-//         .then(response => response.data)
-//         .catch(error => {
-//             if (error.response) {
-//                 throw new Error(`Error: ${error.response.status} - ${error.response.data.message}`);
-//             }
-//             throw new Error(`Error: ${error.message}`);
-//         });
-// };
-
-// export const getRolesByLabelName = (labelName: string): Promise<void> => { 
-//     return axios
-//         .get(`/api/labels/${labelName}/roles`) 
-//         .then(response => response.data)
-//         .catch(error => {
-//             if (error.response) {
-//                 throw new Error(`Error: ${error.response.status} - ${error.response.data.message}`);
-//             }
-//             throw new Error(`Error: ${error.message}`);
-//         });
-// };
-
-// export const removeLabel = (labelName: string): Promise<void> => {
-//     return axios
-//         .delete(`/api/labels/${labelName}`)
-//         .then(response => response.data)
-//         .catch(error => {
-//             if (error.response) {
-//                 throw new Error(`Error: ${error.response.status} - ${error.response.data.message}`);
-//             }
-//             throw new Error(`Error: ${error.message}`);
-//         });
-// };
+export const addRoles = (payload: roles): Promise<void> => {
+    return axios
+        .post(`/api/roles`, payload)
+        .then(response => response.data)
+        .catch(error => {
+            if (error.response) {
+                throw new Error(`Error: ${error.response.status} - ${error.response.data.message}`);
+            }
+            throw new Error(`Error: ${error.message}`);
+        });
+};
