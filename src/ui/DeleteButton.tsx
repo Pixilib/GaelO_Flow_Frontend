ff7655853dcf9638d866755fde651ae5159ffad4
remixxx31 <remi.perray@epitech.eu>
2024-06-14 11:32:12 +0200

    refactor: Create new components EditButton and DeletButton for icons Edit and Delet in Table Closes[Edit | Delete] Create two components For re-use evrywehre we need in the table #220

---
+++ src/ui/DeleteButton.tsx
@@ -0,0 +1,15 @@
+import { BsTrashFill as Delete } from "react-icons/bs";
+
+type DeleteButtonProps = {
+  onClick: () => void;
+}
+
+const DeleteButton = ({ onClick }:DeleteButtonProps) => (
+  <Delete
+    size={'1.3rem'}
+    className="transition duration-70 hover:scale-110"
+    color="#FF0000"
+    onClick={onClick}
+  />
+);
+export default DeleteButton;
