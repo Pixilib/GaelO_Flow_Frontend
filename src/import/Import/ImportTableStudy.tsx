ad61533167869ab66b45fc812ec5e98aacaef93b
Sophie Labyt <labytsophie@gmail.com>
2024-06-27 15:27:01 +0200

    correction tablestudy

--- src/import/Import/ImportTableStudy.tsx
+++ src/import/Import/ImportTableStudy.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from "react";
 import { Table } from "../../ui";
 import { Colors } from "../../utils/enums";
+import DeleteButton from "../../ui/DeleteButton";
 
 interface Patient {
     id: string;
@@ -9,9 +10,10 @@ interface Patient {
 
 interface ImportTableStudyProps {
     data: Patient[];
+    onDelete: (id: string) => void;
 }
 
-const ImportTableStudy: React.FC<ImportTableStudyProps> = ({ data = [] }) => {
+const ImportTableStudy: React.FC<ImportTableStudyProps> = ({ data = [], onDelete }) => {
     const rows = useMemo(() => data, [data]);
 
     const columns = useMemo(() => {
@@ -22,19 +24,17 @@ const ImportTableStudy: React.FC<ImportTableStudyProps> = ({ data = [] }) => {
                 cell: (info: { getValue: () => string; }) => <span>{info.getValue() as string}</span>
             },
             {
-                header: "Select",
-                id: "select",
-                cell: () => (
-                
-                    // <Checkbox
-                    //     checked={row.getIsSelected()}
-                    //     onChange={row.getToggleSelectedHandler()}
-                    // />
-                    null
+                header: "Actions",
+                id: "actions",
+                cell: (info: { row: { original: Patient } }) => (
+                    <DeleteButton
+                        className="px-2 py-1 text-white bg-red-500 rounded"
+                        onClick={() => onDelete(info.row.original.id)}
+                    />
                 ),
             },
         ];
-    }, []);
+    }, [onDelete]);
 
     return (
         <Table
