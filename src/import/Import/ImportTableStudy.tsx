aa2b69916994ff54d57c279d951b0144eca1cf96
Sophie Labyt <labytsophie@gmail.com>
2024-07-01 11:50:16 +0200

    wip

--- src/import/Import/ImportTableStudy.tsx
+++ src/import/Import/ImportTableStudy.tsx
@@ -1,14 +1,14 @@
 import React, { useMemo } from "react";
 import { Table } from "../../ui";
 import { Colors } from "../../utils/enums";
-import Study from "../../model/Study";
+import { Study } from "../../utils/types";
 
 interface ImportTableStudyProps {
     data: Study[];
     onStudyClick: (studyInstanceUID: string) => void
 }
 
-const ImportTableStudy: React.FC<ImportTableStudyProps> = ({ data = [] }) => {
+const ImportTableStudy: React.FC<ImportTableStudyProps> = ({ data = [], onStudyClick }) => {
     const rows = useMemo(() => data, [data]);
 
     const columns = useMemo(() => {
@@ -16,34 +16,41 @@ const ImportTableStudy: React.FC<ImportTableStudyProps> = ({ data = [] }) => {
             {
                 accessorKey: "patientMainDicomTags.patientID",
                 header: "Patient ID",
-                cell: (info: { getValue: () => string; }) => <span>{info.getValue() as string}</span>
+                cell: (info: { getValue: () => any; }) => <span>{info.getValue() as string}</span>
             },
             {
                 accessorKey: "patientMainDicomTags.patientName",
                 header: "Patient Name",
-                cell: (info: { getValue: () => string; }) => <span>{info.getValue() as string}</span>
+                cell: (info: { getValue: () => any; }) => <span>{info.getValue() as string}</span>
             },
             {
                 accessorKey: "mainDicomTags.studyDescription",
                 header: "Study Description",
-                cell: (info: { getValue: () => string; }) => <span>{info.getValue() as string}</span>
+                cell: (info: { getValue: () => any; }) => <span>{info.getValue() as string}</span>
             },
             {
                 accessorKey: "mainDicomTags.studyDate",
                 header: "Study Date",
-                cell: (info: { getValue: () => string; }) => <span>{info.getValue() as string}</span>
+                cell: (info: { getValue: () => any; }) => <span>{info.getValue() as string}</span>
             },
             {
                 accessorKey: "mainDicomTags.accessionNumber",
                 header: "Accession Number",
-                cell: (info: { getValue: () => string; }) => <span>{info.getValue() as string}</span>
+                cell: (info: { getValue: () => any; }) => <span>{info.getValue() as string}</span>
             }
 
         ];
     }, []);
 
     return (
-<div></div>
+        <Table
+            onRowClick={(study: Study) => onStudyClick(study.mainDicomTags.studyInstanceUID)}
+            columns={columns}
+            data={rows}
+            headerColor={Colors.almond}
+            enableColumnFilters
+            enableSorting
+        />
     );
 };
 
