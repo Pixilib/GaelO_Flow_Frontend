2c6de3087dfa072a6277a7454fcce4c1a101a534
remixxx31 <remi.perray@epitech.eu>
2024-07-04 14:29:24 +0200

    refacto: Add RetrieveButton component for retrieving data

---
+++ src/query/RetrieveButton.tsx
@@ -0,0 +1,56 @@
+import { BiDownload } from 'react-icons/bi';
+import { Colors, useCustomMutation } from '../utils';
+import { MouseEvent, useState } from 'react';
+import ProgressJobs from './ProgressJobs';
+import { makeRetrieve } from '../services';
+
+
+/**
+ * A button component used for retrieving data. 
+ * Retrieve data when the button is clicked
+ * Retrieve studies or series data.
+ * @param answerId - The ID of the answer.
+ * @param answerNumber - The number of the answer.
+ * @returns The RetrieveButton component.
+*/
+type RetrieveButtonProps = {
+  answerId: string;
+  answerNumber: number;
+};
+const RetrieveButton = ({ answerId, answerNumber }: RetrieveButtonProps) => {
+
+  const [jobId, setJobId] = useState<string | null>(null);
+
+  const { mutateAsync: retrieveMutation } = useCustomMutation<any, any>(
+    () => makeRetrieve(answerId, answerNumber),
+    [],
+    {
+      onSuccess: (data) => {
+        setJobId(data.ID);
+      }
+    }
+  );
+
+  const handleClick = (event: MouseEvent<SVGAElement>) => {
+    event.stopPropagation()
+    retrieveMutation({})
+  }
+
+  return (
+    <div className="flex justify-center">
+      {jobId ?
+        <ProgressJobs jobId={jobId} />
+        :
+        <BiDownload
+          color={Colors.warning}
+          onClick={handleClick}
+          size="2.5em"
+          className="transition-transform transform cursor-pointer hover:text-warning-500 hover:scale-110"
+        />
+      }
+
+    </div>
+  );
+};
+
+export default RetrieveButton;
