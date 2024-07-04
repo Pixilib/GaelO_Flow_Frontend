46c7e875471a76d717ea4d0db96c6239400676a4
remixxx31 <remi.perray@epitech.eu>
2024-07-04 14:30:29 +0200

    feat: Add RetrieveButton component for retrieving data in SeriesTable

--- src/query/SeriesTable.tsx
+++ src/query/SeriesTable.tsx
@@ -1,12 +1,13 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { Table } from "../ui";
 import { Colors } from "../utils";
+import RetrieveButton from './RetrieveButton';
 
 type SeriesTableProps = {
     series: any[] | null;
 };
 
-const SeriesTable = ({ series }:SeriesTableProps) => {
+const SeriesTable = ({ series }: SeriesTableProps) => {
 
     const columns: ColumnDef<any>[] = [
         {
@@ -25,16 +26,30 @@ const SeriesTable = ({ series }:SeriesTableProps) => {
             accessorKey: "NumberOfSeriesRelatedInstances",
             header: "Instances",
         },
+        {
+            header: "Action",
+            cell: ({ row }: { row: any }) => {
+                return (
+                    console.log(row.original),
+                    <div className="flex justify-center">
+                        <RetrieveButton
+                            answerId={row.original.AnswerId}
+                            answerNumber={row.original.AnswerNumber}
+                        />
+                    </div>
+                )
+            }
+        }
     ];
-
     return (
         <Table
             columns={columns}
             data={series ?? []}
             headerColor={Colors.almond}
             headerTextSize="xs"
+            className="text-xs"
         />
     );
-    
+
 }
 export default SeriesTable;
