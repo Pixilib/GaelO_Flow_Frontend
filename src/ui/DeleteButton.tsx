64fdf9046d4109d52aa89e72f626018fb1b3e2a2
remixxx31 <remi.perray@epitech.eu>
2024-07-11 17:01:29 +0200

    refacto: Update DeleteButton and EditButton to use fill classes for color

--- src/ui/DeleteButton.tsx
+++ src/ui/DeleteButton.tsx
@@ -7,8 +7,7 @@ type DeleteButtonProps = {
 const DeleteButton = ({ onClick }: DeleteButtonProps) => (
   <Delete
     size={'1.3rem'}
-    className="transition duration-70 hover:scale-110"
-    color="#FF0000"
+    className="transition duration-70 hover:scale-110 fill-danger"
     onClick={(e) => {
       e.stopPropagation();
       onClick();
--- src/ui/EditButton.tsx
+++ src/ui/EditButton.tsx
@@ -8,8 +8,7 @@ type EditButtonProps = {
 const EditButton = ({ onClick }: EditButtonProps) => (
   <Edit
     size={'1.3rem'}
-    className="transition duration-70 hover:scale-110"
-    color={Colors.warning}
+    className="transition duration-70 hover:scale-110 fill-warning"
     onClick={(e) => {
       e.stopPropagation();
       onClick();
