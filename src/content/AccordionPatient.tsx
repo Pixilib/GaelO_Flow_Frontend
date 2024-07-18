85716d0cf747d8ba0ff8c483dd73672f2dd4b1cd
remixxx31 <remi.perray@epitech.eu>
2024-07-18 17:14:23 +0200

    refacto: Update AccordionPatient component to use SeriesTable Update onDEletePatient docs: Create stories for AccordionPatient

--- src/content/AccordionPatient.tsx
+++ src/content/AccordionPatient.tsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import { Accordion, DeleteButton, EditButton } from "../ui";
 import { Series } from "../utils/types";
@@ -7,22 +6,28 @@ import { getSeriesOfStudy } from "../services/orthanc";
 import Patient from "../model/Patient";
 import Study from "../model/Study";
 import StudyTable from "./StudyTable";
-import SeriesTable from "../query/SeriesTable";
+import SeriesTable from "../content/SeriesTable";
 
 type AccordionPatientProps = {
     patient: Patient;
-    onEditPatient: (e: React.MouseEvent<HTMLButtonElement | SVGElement>, patientId: string) => void;
-    onDeletePatient: (e: React.MouseEvent<HTMLButtonElement | SVGElement>, patientId: string) => void;
+    onEditPatient: (patient: Patient) => void;
+    onDeletePatient: (patientId: string) => void;
 };
 
 const AccordionPatient = ({ patient, onEditPatient, onDeletePatient }: AccordionPatientProps) => {
     const [currentStudyId, setCurrentStudyId] = useState<string | null>(null);
 
-    const { data: series } = useCustomQuery<Series[]>(
-        ['studies', currentStudyId as string, 'series'],
+    const { data: seriesByStudy, isLoading, isError } = useCustomQuery<Series[]>(
+        ["studies", currentStudyId as string, "series"],
         () => getSeriesOfStudy(currentStudyId as string),
         {
-            enabled: currentStudyId != null,
+            enabled: !!currentStudyId,
+            onSuccess: data => {
+                console.log("Fetched series:", data);
+            },
+            onError: error => {
+                console.error("Error fetching series:", error);
+            }
         }
     );
 
@@ -33,21 +38,27 @@ const AccordionPatient = ({ patient, onEditPatient, onDeletePatient }: Accordion
     return (
         <Accordion
             summary={
-                <div className="flex items-center justify-between w-full gap-x-10">
-                    <span className="text-lg font-medium">Patient ID: {patient.patientId}</span>
+                <div className="flex items-center justify-between w-full lg:gap-x-10">
+                    <span className="text-sm font-medium text-primary lg:text-lg">Patient ID: {patient.patientId}</span>
                     <span className="text-sm">Name: {patient.patientName}</span>
                     <span className="text-sm">Nb of Studies: {patient.getStudies().length}</span>
                     <div className="flex space-x-7">
-                        <EditButton onClick={(e) => onEditPatient(e, patient.id)} />
-                        <DeleteButton onClick={(e) => onDeletePatient(e, patient.id)} />
+                        <EditButton onClick={() => onEditPatient(patient)} />
+                        <DeleteButton 
+                            onClick={() => {onDeletePatient(patient.id)}} 
+                        />
                     </div>
                 </div>
             }
             variant="primary"
             className="w-full rounded-2xl"
         >
-            <StudyTable studies={patient.getStudies()} onRowClick={handleOnRowClick} />
-            {currentStudyId && <SeriesTable series={series ?? []} />}
+            {isLoading && <p>Loading series...</p>}
+            {isError && <p>Error loading series.</p>}
+            <div className={`${currentStudyId ? 'grid grid-cols-1 gap-y-3 xl:grid-cols-2 xl:gap-x-2' : 'grid grid-cols-1'}`}>
+                <StudyTable studies={patient.getStudies()} onRowClick={handleOnRowClick} />
+                {seriesByStudy && <SeriesTable series={seriesByStudy} />}
+            </div>
         </Accordion>
     );
 };
