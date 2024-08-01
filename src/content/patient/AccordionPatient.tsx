a3b0db478e91bbe04d6832e552c5e13bdf5e2f10
remixxx31 <remi.perray@epitech.eu>
2024-08-01 17:21:53 +0200

    refacto: Update AccordionPatient component to improve code readability

--- src/content/patient/AccordionPatient.tsx
+++ src/content/patient/AccordionPatient.tsx
@@ -1,36 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { deletePatient, useConfirm } from "../../services";
+import { useCustomMutation, useCustomToast } from "../../utils";
 import { Accordion, DeleteButton, EditButton } from "../../ui";
+
 import Patient from "../../model/Patient";
-import StudiesManager from "../StudiesManager";
+
+import EditPatient from "./EditPatient";
+import StudyRoot from "../studies/StudyRoot";
+import SeriesRoot from "../series/SeriesRoot";
 
 type AccordionPatientProps = {
     patient: Patient;
-    onEditPatient: (patient: Patient) => void;
-    onDeletePatient: (patientId: string) => void;
+    onPatientEdited: () => void;
+    onPatientDeleted: () => void;
 };
 
-const AccordionPatient: React.FC<AccordionPatientProps> = ({ patient, onEditPatient, onDeletePatient }) => {
-    console.log('AccordionPatient rendering with patient:', patient);
+const AccordionPatient: React.FC<AccordionPatientProps> = ({ patient, onPatientEdited, onPatientDeleted }) => {
+    const { toastSuccess, toastError } = useCustomToast();
+    const { confirm } = useConfirm();
 
-    return (
-        <Accordion
-            summary={
-                <div className="flex items-center justify-between w-full lg:gap-x-10">
-                    <span className="text-sm font-medium text-primary lg:text-lg">Patient ID: {patient.patientId}</span>
-                    <span className="text-sm">Name: {patient.patientName}</span>
-                    <span className="text-sm">Nb of Studies: {patient.getStudies().length}</span>
-                    <div className="flex space-x-7">
-                        <EditButton onClick={() => onEditPatient(patient)} />
-                        <DeleteButton onClick={() => onDeletePatient(patient.id)} />
-                    </div>
-                </div>
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedStudyId, setSelectedStudyId] = useState<string | null>(null);
+
+    const { mutateAsync: mutateDeletePatient } = useCustomMutation<void, string>(
+        (patientId) => deletePatient(patientId),
+        [['jobs']],
+        {
+            onSuccess: async () => {
+                toastSuccess("Patient deleted successfully");
+                onPatientDeleted();
+            },
+            onError: (error: any) => {
+                toastError(`Failed to delete patient ${error}`);
             }
-            variant="primary"
-            className="w-full rounded-2xl"
-        >
-            <StudiesManager patient={patient} />
-        </Accordion>
+        }
+    );
+    const handleDeletePatient = async () => {
+        const confirmContent = (
+            <div className="italic">
+                Are you sure you want to delete this patient:
+                <span className="text-xl not-italic font-bold text-primary">{patient.id} {patient.patientName} ?</span>
+            </div>
+        );
+        if (await confirm({ content: confirmContent })) {
+            mutateDeletePatient(patient.id);
+        }
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const handlePatientUpdate = () => {
+        onPatientEdited();
+        closeModal();
+    };
+
+    const handleStudySelected = (studyId: string) => {
+        setSelectedStudyId(studyId);
+    };
+    return (
+        <>
+            {isModalOpen && (
+                <EditPatient
+                    patient={patient}
+                    onEditPatient={handlePatientUpdate}
+                    onClose={closeModal}
+                    show={isModalOpen}
+                />
+            )}
+            <Accordion
+                summary={
+                    <div className="flex items-center justify-between w-full lg:gap-x-10">
+                        <span className="text-sm font-medium text-primary lg:text-lg">Patient ID: {patient.patientId}</span>
+                        <span className="text-sm">Name: {patient.patientName}</span>
+                        <span className="text-sm">Nb of Studies: {patient.getStudies().length}</span>
+                        <div className="flex space-x-7">
+                            <EditButton onClick={() => setIsModalOpen(true)} />
+                            <DeleteButton onClick={handleDeletePatient} />
+                        </div>
+                    </div>
+                }
+                variant="primary"
+                className="w-full rounded-2xl"
+            >
+                <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+                    <div className={`${!selectedStudyId ? 'lg:col-span-2' : ''}`}>
+                        <StudyRoot
+                            patient={patient}
+                            onStudyUpdated={onPatientEdited}
+                            onStudySelected={handleStudySelected}
+                        />
+                    </div>
+                    {selectedStudyId && (
+                        <div>
+                            <SeriesRoot
+                                studyId={selectedStudyId}
+                                onSeriesUpdate={onPatientEdited}
+                            />
+                        </div>
+                    )}
+                </div>
+            </Accordion>
+        </>
     );
 };
 
 export default AccordionPatient;
+
+
