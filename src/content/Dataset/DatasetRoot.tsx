d42cec9509c13b9987d4b46fdb5f9e1685a0fc7c
Sophie Labyt <labytsophie@gmail.com>
2024-08-01 16:12:11 +0200

    WIP DatasetSelect

--- src/content/Dataset/DatasetRoot.tsx
+++ src/content/Dataset/DatasetRoot.tsx
@@ -6,16 +6,13 @@ const DatasetRoot = () => {
         <Card>
             <CardHeader
           className="flex items-center justify-center rounded-t-lg text-bg-light"
-          color={Colors.primary} title={''}            />
+          color={Colors.primary} title={'Dataset'}            />
             <CardBody>
                 <div>
-                    <div>
-                        {/* Contenido del cuerpo de la tarjeta aquí */}
-                    </div>
+                    
                 </div>
             </CardBody>
             <CardFooter className="flex justify-center border-t-2 border-indigo-100 shadow-inner bg-light">
-                {/* Contenido del pie de la tarjeta aquí */}
             </CardFooter>
         </Card>
     );
