a410f070539ac37705cab0cd0da90697641a3007
remixxx31 <remi.perray@epitech.eu>
2024-08-01 18:01:48 +0200

    refacto: Update Study model to include studyId and accessionNumber

--- src/model/Study.ts
+++ src/model/Study.ts
@@ -1,4 +1,4 @@
-import { getStudies } from "../services/orthanc"
+import { getStudy } from "../services/orthanc"
 import Patient from "./Patient"
 import Series from "./Series"
 import type { Study as StudyType } from './../utils/types'
@@ -6,9 +6,11 @@ import type { Study as StudyType } from './../utils/types'
 class Study {
 
     id: string
+    studyId: string | null = null
     studyDescription: string | null = null
     studyDate: string | null = null
     studyTime: string | null = null
+    accessionNumber: string | null = null
     studyInstanceUID?: string
     patient: Patient | null = null
     series: Series[]
@@ -19,11 +21,13 @@ class Study {
     }
 
     async fillFromOrthanc() {
-        const study = await getStudies(this.id)
+        const study = await getStudy(this.id)
         this.studyDescription = study.mainDicomTags.studyDescription;
         this.studyDate = study.mainDicomTags.studyDate;
         this.studyTime = study.mainDicomTags.studyTime;
         this.studyInstanceUID = study.mainDicomTags.studyInstanceUID;
+        this.accessionNumber = study.mainDicomTags.accessionNumber;
+        this.studyId = study.mainDicomTags.studyId;
         this.patient = new Patient(study.parentPatient)
         this.patient.setPatientId(study.patientMainDicomTags.patientId)
         this.patient.setPatientName(study.patientMainDicomTags.patientName)
@@ -36,13 +40,15 @@ class Study {
         this.studyDate = studyData.mainDicomTags.studyDate;
         this.studyTime = studyData.mainDicomTags.studyTime;
         this.studyInstanceUID = studyData.mainDicomTags.studyInstanceUID;
+        this.accessionNumber = studyData.mainDicomTags.accessionNumber;
+        this.studyId = studyData.mainDicomTags.studyId;
 
     }
 
     setPatient =
-     (patient: Patient) => {
-        this.patient = patient
-    }
+        (patient: Patient) => {
+            this.patient = patient
+        }
 
     getAllseries = () => {
         return this.series.map(series => series.toJSON())
@@ -69,13 +75,15 @@ class Study {
         return this.studyInstanceUID
     }
 
-    toJSON = (): object => {
+    toJSON = () => {
         return {
             'id': this.id,
             'studyDescription': this.studyDescription,
             'studyDate': this.studyDate,
             'studyTime': this.studyTime,
-            'studyInstanceUID': this.studyInstanceUID,
+            'studyId': this.studyId,
+            'accessionNumber': this.accessionNumber,
+            'studyInstanceUID': this.studyInstanceUID as string,
             'series': this.series.map(series => series.toJSON()),
             'patient': this.patient?.id
         }
