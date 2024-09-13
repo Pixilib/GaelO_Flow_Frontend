9dcb729aa9265c382b35c9f81559709137d11803
Salim Kanoun <salim.kanoun@gmail.com>
2024-09-13 23:51:48 +0200

    update tags

--- src/content/series/Tags.tsx
+++ src/content/series/Tags.tsx
@@ -12,26 +12,28 @@ const Tags = ({ seriesId }: TagsProps) => {
     const { data: instances } = useCustomQuery(['series', seriesId, 'instances'], () => getInstancesOfSeries(seriesId))
     const [instanceNumber, setInstanceNumber] = useState<number | null>(null)
 
-    const currentInstanceId = instanceNumber!=null ? instances[instanceNumber].id : null
+    const currentInstanceId = instanceNumber != null ? instances[instanceNumber].id : null
 
-    const {data : header}  = useCustomQuery(
+    const { data: header } = useCustomQuery(
         ['instances', currentInstanceId, 'metadata'],
-        () => instanceHeader(instances[instanceNumber].id),{
-            enabled : (instanceNumber !== null)
+        () => instanceHeader(instances[instanceNumber].id),
+        {
+            enabled: (instanceNumber !== null)
         }
     )
 
-    const {data : tags}  = useCustomQuery(
+    const { data: tags } = useCustomQuery(
         ['instances', currentInstanceId, 'tags'],
-        () => instanceTags(instances[instanceNumber].id),{
-            enabled : (instanceNumber !== null)
+        () => instanceTags(instances[instanceNumber].id),
+        {
+            enabled: (instanceNumber !== null)
         }
     )
 
     if (!instances) return <Spinner />
     return (
         <>
-            <Input min={1} max={instances.length} value={instanceNumber ?? 0 }  onChange={(event) => setInstanceNumber(Number(event.target?.value))} />
+            <Input min={1} max={instances.length} value={instanceNumber ?? 0} onChange={(event) => setInstanceNumber(Number(event.target?.value))} />
             <pre>
                 {JSON.stringify(header, null, 2)}
                 {JSON.stringify(tags, null, 2)}
