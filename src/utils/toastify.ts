d5080d991f7c0a5f6d399d1bfb5403943b774016
Salim Kanoun <salim.kanoun@gmail.com>
2024-09-14 19:43:56 +0200

    prepare update

--- src/utils/toastify.ts
+++ src/utils/toastify.ts
@@ -1,23 +1,27 @@
 
 import useToasts from '../services/useToasts'
 export const useCustomToast = () => {
-    const { pushToast } = useToasts()
+    const { pushToast, updateToast } = useToasts()
 
     const toastSuccess = (message: string) => {
-        pushToast({ content: message, type: 'success', duration: 1.3, position: 'bottom-right', animation: 'slide-left' })
+        return pushToast({ content: message, type: 'success', duration: 1.3, position: 'bottom-right', animation: 'slide-left' })
     }
 
     const toastWarning = (message: string) => {
-        pushToast({ content: message, type: 'warning', duration: 1.3, position: 'bottom-right', animation: 'slide-left' })
+        return pushToast({ content: message, type: 'warning', duration: 1.3, position: 'bottom-right', animation: 'slide-left' })
     }
 
     const toastError = (message: string) => {
-        pushToast({ content: message, type: 'danger', duration: 1.3, position: 'bottom-right', animation: 'slide-left' })
+        return pushToast({ content: message, type: 'danger', duration: 1.3, position: 'bottom-right', animation: 'slide-left' })
     }
 
     const toastInfo = (message: string) => {
-        pushToast({ content: message, type: 'info', duration: 1.3, position: 'bottom-right', animation: 'slide-left' })
+        return pushToast({ content: message, type: 'info', duration: 1.3, position: 'bottom-right', animation: 'slide-left' })
     }
 
-    return { toastSuccess, toastWarning, toastError, toastInfo }
+    const updateExistingToast = (id :number, message : string)=>{
+        updateToast(id, {content : message})
+    }
+
+    return { toastSuccess, toastWarning, toastError, toastInfo, updateExistingToast }
 }
