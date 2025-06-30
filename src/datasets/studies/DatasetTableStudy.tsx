e9b29cd6814315290578418eba0273fe87eb4797
Salim Kanoun <salim.kanoun@gmail.com>
2025-06-30 10:11:39 +0200

    add patient id and patient name in study table

--- src/datasets/studies/DatasetTableStudy.tsx
+++ src/datasets/studies/DatasetTableStudy.tsx
@@ -6,66 +6,77 @@ import DatasetStudyActions from "./DatasetStudyActions";
 import Study from "../../model/Study";
 
 type StudyTableProps = {
-    studies: Study[];
-    onRowClick: (studyId: string) => void;
-    onActionClick: (action: string, studyId: string) => void;
-    selectedStudyId: string | null;
+  studies: Study[];
+  onRowClick: (studyId: string) => void;
+  onActionClick: (action: string, studyId: string) => void;
+  selectedStudyId: string | null;
 };
 
 const DatasetTableStudy: React.FC<StudyTableProps> = ({
-    studies,
-    onRowClick,
-    onActionClick,
-    selectedStudyId,
+  studies,
+  onRowClick,
+  onActionClick,
+  selectedStudyId,
 }) => {
+  const columns: ColumnDef<Study>[] = useMemo(
+    () => [
+      {
+        id: "id",
+      },
+      {
+        accessorKey: "patient.patientId",
+        header: "Patient ID",
+      },
+      {
+        accessorKey: "patient.patientName",
+        header: "Patient Name",
+      },
+      {
+        accessorKey: "accessionNumber",
+        header: "Accession Number",
+      },
+      {
+        accessorKey: "studyDate",
+        header: "Acquisition Date",
+      },
+      {
+        accessorKey: "studyDescription",
+        header: "Study Description",
+      },
+      {
+        header: "Actions",
+        cell: ({ row }) => {
+          const study = row.original;
+          return (
+            <DatasetStudyActions study={study} onActionClick={onActionClick} />
+          );
+        },
+      },
+    ],
+    [onActionClick]
+  );
 
-    const columns: ColumnDef<Study>[] = useMemo(() => [
-        {
-            id : 'id'
-        },
-        {
-            accessorKey: "accessionNumber",
-            header: "Accession Number",
-        },
-        {
-            accessorKey: "studyDate",
-            header: "Acquisition Date",
-        },
-        {
-            accessorKey: "studyDescription",
-            header: "Study Description",
-        },
-        {
-            header: "Actions",
-            cell: ({ row }) => {
-                const study = row.original;
-                return <DatasetStudyActions study={study} onActionClick={onActionClick} />;
-            },
-        },
-    ], [onActionClick]);
+  const getRowClasses = (study: Study) => {
+    const isSelected = study.id === selectedStudyId;
+    return isSelected
+      ? "bg-primary hover:cursor-pointer text-white font-bold"
+      : "hover:bg-indigo-100 hover:cursor-pointer";
+  };
 
-    const getRowClasses = (study: Study) => {
-        const isSelected = study.id === selectedStudyId;
-        return isSelected
-            ? 'bg-primary hover:cursor-pointer text-white font-bold'
-            : 'hover:bg-indigo-100 hover:cursor-pointer';
-    };
-
-
-    return (
-        <Table
-            columns={columns}
-            data={studies}
-            enableColumnFilters={true}
-            headerColor={Colors.white}
-            onRowClick={(row) => onRowClick(row.id)}
-            enableSorting={true}
-            enableRowSelection={true}
-            headerTextSize='xs'
-            className="text-xs bg-gray-100"
-            getRowClasses={getRowClasses} 
-        />
-    );
+  return (
+    <Table
+      columns={columns}
+      data={studies}
+      enableColumnFilters={true}
+      headerColor={Colors.white}
+      onRowClick={(row) => onRowClick(row.id)}
+      enableSorting={true}
+      enableRowSelection={true}
+      headerTextSize="xs"
+      className="text-xs bg-gray-100"
+      getRowClasses={getRowClasses}
+    />
+  );
 };
 
 export default DatasetTableStudy;
--- src/model/Model.ts
+++ src/model/Model.ts
@@ -1,95 +1,108 @@
-import Instance from "./Instance"
-import Patient from "./Patient"
-import Series from "./Series"
-import Study from "./Study"
-import type { Study as StudyType } from "./../utils/types"
+import Instance from "./Instance";
+import Patient from "./Patient";
+import Series from "./Series";
+import Study from "./Study";
+import type { Study as StudyType } from "./../utils/types";
 
 class Model {
-    patients: Patient[]
+  patients: Patient[];
 
-    constructor() {
-        this.patients = []
+  constructor() {
+    this.patients = [];
+  }
+
+  isPatientIdExists(patientId: string) {
+    const knownPatientIds = this.patients.map((patient) => patient.id);
+    return knownPatientIds.includes(patientId);
+  }
+
+  addPatient(patient: Patient) {
+    const knownPatientIds = this.patients.map((patient) => patient.id);
+    if (!knownPatientIds.includes(patient.id)) {
+      this.patients.push(patient);
+    }
+  }
+
+  getStudies() {
+    const studies = this.patients
+      .map((patient) => patient.getStudies())
+      .flat()
+      .map((study) => study.toJSON());
+    return studies;
+  }
+
+  getStudy(studyInstanceUID: string): Study {
+    const studies = this.patients
+      .map((patient) => patient.getStudies())
+      .flat()
+      .filter((study) => study.studyInstanceUID === studyInstanceUID);
+    if (studies.length === 1) {
+      return studies[0];
+    } else throw "study not found";
+  }
+
+  addStudy(studyData: StudyType) {
+    if (!this.isPatientIdExists(studyData.parentPatient)) {
+      const patient = new Patient(studyData.parentPatient);
+      patient.fillData(studyData.patientMainDicomTags);
+      this.patients.push(patient);
+    }
+    const patient = this.patients.find(
+      (patient) => patient.id === studyData.parentPatient
+    );
+    if (patient && !patient.isStudyExists(studyData.id)) {
+      const study = new Study(studyData.id);
+      study.fillData(studyData);
+      const patientObject = new Patient(studyData.parentPatient);
+      if (studyData?.patientMainDicomTags)
+        patientObject.fillData(studyData.patientMainDicomTags);
+      study.setPatient(patientObject);
+      patient.addStudy(study);
+    }
+  }
+
+  async addInstance(
+    instanceId: string,
+    seriesId: string,
+    studyId: string,
+    patientId: string
+  ) {
+    if (!this.isPatientIdExists(patientId)) {
+      const patient = new Patient(patientId);
+      await patient.fillFromOrthanc();
+      this.patients.push(patient);
     }
 
-    isPatientIdExists(patientId: string) {
-        const knownPatientIds = this.patients.map(patient => patient.id)
-        return knownPatientIds.includes(patientId)
+    const patient = this.patients.find((patient) => patient.id === patientId);
+    if (patient && !patient?.isStudyExists(studyId)) {
+      const study = new Study(studyId);
+      await study.fillFromOrthanc();
+      patient.addStudy(study);
     }
 
-    addPatient(patient: Patient) {
-        const knownPatientIds = this.patients.map(patient => patient.id)
-        if (!knownPatientIds.includes(patient.id)) {
-            this.patients.push(patient)
-        }
+    const study = patient?.getStudy(studyId);
+    if (study && !study?.isSeriesExists(seriesId)) {
+      const series = new Series(seriesId);
+      await series.fillFromOrthanc();
+      study.addSeries(series);
     }
 
-    getStudies() {
-        const studies = this.patients.map(patient => patient.getStudies()).flat().map(study => study.toJSON())
-        return studies
+    const series = study?.getSeries(seriesId);
+    if (series && !series?.isInstanceExists(instanceId)) {
+      const instance = new Instance(instanceId);
+      series.addInstance(instance);
     }
+  }
 
-    getStudy(studyInstanceUID: string): Study {
-        const studies = this.patients.map(patient => patient.getStudies()).flat().filter(study => study.studyInstanceUID === studyInstanceUID)
-        if (studies.length === 1) {
-            return studies[0]
-        }
-        else throw "study not found"
-    }
+  getPatients = () => {
+    return this.patients;
+  };
 
-    addStudy(studyData: StudyType) {
-        if (!this.isPatientIdExists(studyData.parentPatient)) {
-            const patient = new Patient(studyData.parentPatient)
-            patient.fillData(studyData.patientMainDicomTags)
-            this.patients.push(patient)
-        }
-        const patient = this.patients.find(patient => patient.id === (studyData.parentPatient))
-        if (patient && !patient.isStudyExists(studyData.id)) {
-            const study = new Study(studyData.id)
-            study.fillData(studyData)
-            study.setPatient(new Patient(studyData.parentPatient))
-            patient.addStudy(study)
-        }
-    }
-
-    async addInstance (instanceId: string, seriesId: string, studyId: string, patientId: string) {
-        if (!this.isPatientIdExists(patientId)) {
-            const patient = new Patient(patientId)
-            await patient.fillFromOrthanc()
-            this.patients.push(patient)
-        }
-
-        const patient = this.patients.find(patient => patient.id === patientId)
-        if (patient && !patient?.isStudyExists(studyId)) {
-            const study = new Study(studyId)
-            await study.fillFromOrthanc()
-            patient.addStudy(study)
-
-        }
-
-        const study = patient?.getStudy(studyId)
-        if (study && !study?.isSeriesExists(seriesId)) {
-            const series = new Series(seriesId)
-            await series.fillFromOrthanc()
-            study.addSeries(series)
-        }
-
-        const series = study?.getSeries(seriesId)
-        if (series && !series?.isInstanceExists(instanceId)) {
-            const instance = new Instance(instanceId)
-            series.addInstance(instance)
-        }
-
-    }
-
-    getPatients = () => {
-        return this.patients
-    }
-
-    toJSON = () => {
-        return {
-            'patients': this.patients.map(patient => patient.toJSON()),
-        }
-    }
+  toJSON = () => {
+    return {
+      patients: this.patients.map((patient) => patient.toJSON()),
+    };
+  };
 }
 
-export default Model
+export default Model;
